33336601107274db650e8c1e2dcdfd4712e3ff8a
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2024-11-24 22:57:18 +0800

    Set up environment variable and add vercel.json configuration

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,9 +2,8 @@ import Navbar from "../components/navbar";
 import Footer from "../components/Footer";
 import UserInfo from "../components/Account/UserInfo";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import GOOGLE_CLIENT_ID from "../../config";
 
-const clientId = GOOGLE_CLIENT_ID;
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID; 
 
 function Account() {
   return (
