b44039a29c4dfd00b088f638c0b640474e19a68b
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2025-05-18 14:10:44 +0800

    Fix background inconsistency

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,7 +4,7 @@
   export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex w-screen h-fit">
+      <div className="flex w-screen h-fit bg-gray-50">
         <AppSidebar />
         <SidebarTrigger />
         <main className="flex-1 flex flex-col items-center">
